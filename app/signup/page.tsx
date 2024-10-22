60000befc7e2fb7c03e2f9c9af122903e7bfe2d3
Allen <allenshamrock37@gmail.com>
2024-10-22 12:28:21 +0300

    {Enh}:Implemented the signup page feature

---
+++ app/signup/page.tsx
@@ -0,0 +1,8 @@
+import Forms from "@/components/Forms";
+export default function signup(){
+    return(
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <Forms/>
+        </div>
+    )
+}
