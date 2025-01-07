e45194593d45926605730634ab97bfe78d8c89bc
Allen Shamrock <allenshamrock37@gmail.com>
2025-01-07 10:42:50 +0300

    {Enh}:Restructered the uploadthing action file

--- actions/uploadThingsActions.ts
+++ actions/uploadThingsActions.ts
@@ -4,15 +4,15 @@ import { UTApi } from "uploadthing/server";
 
 const utapi = new UTApi();
 
-export async function deleteImage(key:string){
-    const user = await getUserSession()
-    if(!user || user.role==='admin'){
-        throw new Error("Unathorized access!")
-    }
+export async function deleteImage(key: string) {
+  const user = await getUserSession();
+  if (!user || user.role !== "admin") {
+    throw new Error("Unathorized access!");
+  }
 
-    try{
-        await utapi.deleteFiles(key)
-    }catch(error){
-        console.log(error)
-    }
+  try {
+    await utapi.deleteFiles(key);
+  } catch (error) {
+    console.log(error);
+  }
 }
